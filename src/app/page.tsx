ed7329cfecc780fb27d8781f209eef1bbd59f7a5
Giovane <98927748+Giovanecunhadias@users.noreply.github.com>
2024-11-23 00:33:14 -0300

    add new film to list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,11 @@ export default async function Home() {
     <HydrateClient>
       <div>
       
-        {session?.user && <Link href={"https://embed.warezcdn.link/serie/tt0898266"}><Image src="https://m.media-amazon.com/images/M/MV5BZjgzY2QyNzItNDhhYi00ZWIwLWFjN2UtZDJkN2MxYWNjMmJjXkEyXkFqcGc@._V1_SX300.jpg" alt="logo" width={200} height={200} /></Link> }
+        {session?.user && <>
+          <Link href={"https://embed.warezcdn.link/serie/tt0898266"}><Image src="https://m.media-amazon.com/images/M/MV5BZjgzY2QyNzItNDhhYi00ZWIwLWFjN2UtZDJkN2MxYWNjMmJjXkEyXkFqcGc@._V1_SX300.jpg" alt="logo" width={200} height={200} /></Link> 
+          <Link href={"https://embed.warezcdn.link/filme/tt9218128"}><Image src="https://m.media-amazon.com/images/M/MV5BMWYzZTM5ZGQtOGE5My00NmM2LWFlMDEtMGNjYjdmOWM1MzA1XkEyXkFqcGc@._V1_SX300.jpg" alt="logo" width={200} height={200} /></Link> 
+        
+        </>}
         {!session?.user && (
           <div>
             <Link href="api/auth/signin">Login</Link>
