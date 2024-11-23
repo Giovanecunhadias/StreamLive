cacee6ecd08ff3962e53044042e330aac70a2fc6
Giovane <98927748+Giovanecunhadias@users.noreply.github.com>
2024-11-23 00:00:27 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+
+import { auth } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+import Image from "next/image";
+export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <div>
+      
+        {session?.user && <Link href={"https://embed.warezcdn.link/serie/tt0898266"}><Image src="https://m.media-amazon.com/images/M/MV5BZjgzY2QyNzItNDhhYi00ZWIwLWFjN2UtZDJkN2MxYWNjMmJjXkEyXkFqcGc@._V1_SX300.jpg" alt="logo" width={200} height={200} /></Link> }
+        {!session?.user && (
+          <div>
+            <Link href="api/auth/signin">Login</Link>
+          </div>
+        )}
+        {session?.user && <Link href="api/auth/signout">Logout</Link>}
+      </div>
+    </HydrateClient>
+  );
+}
