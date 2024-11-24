57c37c5108222b64d3a64ea36e6d53f2a830d1a0
Giovane <98927748+Giovanecunhadias@users.noreply.github.com>
2024-11-24 16:55:38 -0300

    bug fix in routes

--- src/app/live/LiveClient.tsx
+++ src/app/live/LiveClient.tsx
@@ -2,7 +2,14 @@
 
 import React, { useEffect } from "react";
 
-export default function LiveClient({ session }: { session: any }) {
+// Define a interface para a sessão
+interface Session {
+    user?: {
+        id: string;
+    };
+}
+
+export default function LiveClient({ session }: { session: Session }) {
     useEffect(() => {
         if (!session?.user) {
             // Redireciona para a página inicial se não houver usuário autenticado
@@ -17,7 +24,7 @@ export default function LiveClient({ session }: { session: any }) {
     return (
         <>
             <h1>Live</h1>
-            <div>Em breve você poderá streamar suas próprias lives</div>
+            <div>Em breve você poderá streamar suas próprias lives.</div>
         </>
     );
 }
